08d1ead0d0e0fe259b76b74ae2993f3b6a1e4d55
Ian Lawrie <iplawrie@mtu.edu>
2024-08-24 13:28:46 -0400

    added automatic ascension and upgrade buys

--- CookieClicker.js
+++ CookieClicker.js
@@ -3,12 +3,34 @@ var cookieClickerInterval;
 var buildingPurchaseInterval;
 var checkPrestigeInterval;
 var havePrestiged = false;
+var maxPrestige = 100000
+var minHeavenlyCookies = 0
 
 function buyPrestigeUpgrade(){
-
+	var availableprestigeupgrades = Game.PrestigeUpgrades.filter(upgrade => upgrade.canBePurchased && !upgrade.bought);
+	var sortedupgrades = availableprestigeupgrades.sort(prestigeCompare);
+	for (let upgrade in sortedupgrades) {
+		if(!upgrade.buy()){
+			minAscend = upgrade.basePrice + sortedupgrades[sortedupgrades.indexOf(upgrade)+1].basePrice;
+			break;
+		}
+	}
+	Game.Reincarnate(true)
+}
+function prestigeCompare(a,b){
+	if(a.basePrice < b.basePrice){return -1;}
+    if(a.basePrice > b.basePrice){return 1;}
+    return 0
 }
 
 function automateCookieClicker(){
+
+	//find where prestige left off at
+	if(Game.prestige){
+		var availableprestigeupgrades = Game.PrestigeUpgrades.filter(upgrade => upgrade.canBePurchased && !upgrade.bought);
+		var sortedupgrades = availableprestigeupgrades.sort(prestigeCompare);
+		minHeavenlyCookies = sortedupgrades[0].basePrice + sortedupgrades[1].basePrice;
+	}
 	//clicks big cookie 5ms
 	cookieClickerInterval = setInterval(Game.ClickCookie, 5);
 	
@@ -32,15 +54,17 @@ function automateCookieClicker(){
 			console.log("Bought " + buybuilding.name + " building at " + buybuildingprice);
 		}
 	}, 1000);
-/*
+
 	//check for prestige & buy available prestige upgrades
 	var checkPrestigeInterval = setInterval(function(){
 		if(!Game.prestige && Game.ascendMeterLevel >= 5){
-			Game.Ascend(1)
-			buyPrestigeUpgrade()
+			Game.Ascend(true);
+			buyPrestigeUpgrade();
+		}else if(Game.ascendMeterLevel >= maxPrestige || Game.heavenlyChips >= minHeavenlyCookies){
+			Game.Ascend(true);
+			buyPrestigeUpgrade();
 		}
 	}, 1000*60*60);
-*/
 	
 }
 function stopAll(){
